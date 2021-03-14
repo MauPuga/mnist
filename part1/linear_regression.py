56c64977f899931adc882014b32b6c1a1cbf02c1
Mauricio Martinez Puga <ammartinezp@indracompany.com>
2021-03-14 12:23:23 -0600

    tarea 1 y 2

--- part1/linear_regression.py
+++ part1/linear_regression.py
@@ -15,7 +15,9 @@ def closed_form(X, Y, lambda_factor):
         theta - (d + 1, ) NumPy array containing the weights of linear regression. Note that theta[0]
         represents the y-axis intercept of the model and therefore X[0] = 1
     """
-    # YOUR CODE HERE
+    A = X.T.dot(X) + lambda_factor * np.identity(X.shape[1])
+    theta = np.linalg.inv(A).dot(X.T.dot(Y))
+    return theta
     raise NotImplementedError
 
 ### Functions which are already complete, for you to use ###
--- part1/test.py
+++ part1/test.py
@@ -17,13 +17,13 @@ verbose = False
 epsilon = 1e-6
 
 def green(s):
-    return '\033[1;32m%s\033[m' % s
+    return '[1;32m%s[m' % s
 
 def yellow(s):
-    return '\033[1;33m%s\033[m' % s
+    return '[1;33m%s[m' % s
 
 def red(s):
-    return '\033[1;31m%s\033[m' % s
+    return '[1;31m%s[m' % s
 
 def log(*m):
     print(" ".join(map(str, m)))
